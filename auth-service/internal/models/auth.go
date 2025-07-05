15066cc30f95d9e6abbffa3063464fe0bbf29437
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-07-05 18:40:23 +0300

    user models

---
+++ auth-service/internal/models/auth.go
@@ -0,0 +1,43 @@
+package models
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID          uuid.UUID `json:"id"`
+	FirstName   string    `json:"firstname"`
+	LastName    string    `json:"lastname"`
+	Email       string    `json:"email"`
+	Password    string    `json:"password"`
+	PhoneNumber int32     `json:"phonenumber"`
+	Metadata    Metadata  `json:"metadata"`
+	CreatedAT   time.Time `json:"crreatedat"`
+	UpdatedAT   time.Time `json:"updatedat"`
+}
+
+type Metadata struct {
+	Location string `json:"location"`
+}
+
+func NewUser(id uuid.UUID, firstname string, lastname string, password string, email string) (*User, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("HASH_ERROR:Failed to harsh password:%v", err)
+	}
+	return &User{
+		ID:        id,
+		FirstName: firstname,
+		LastName:  lastname,
+		Email:     email,
+		Password:  string(hashedPassword),
+	}, nil
+}
+
+func (u *User) ComparePassword(password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+}
