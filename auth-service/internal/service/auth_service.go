f32bbe355fb46784f6df4acc86f8e9250a7181ad
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-07-06 15:51:44 +0300

    register user transaction service

--- auth-service/internal/service/auth_service.go
+++ auth-service/internal/service/auth_service.go
@@ -1,7 +1,55 @@
 package service
 
-import "sports/authservice/internal/database"
+import (
+	"context"
+	"errors"
+	"sports/authservice/internal/database"
+	"sports/authservice/internal/models"
+
+	"github.com/google/uuid"
+)
+
+var (
+	ErrEmailExists = errors.New("email already esists")
+	ErrNotFound    = errors.New("email does not exists")
+)
 
 type AuthService struct {
 	db database.DBInterface
+}
+
+func NewAuthService(db database.DBInterface) *AuthService {
+	return &AuthService{db}
+}
+
+func (s *AuthService) Register(ctx context.Context, id uuid.UUID, firstname string, lastname string, email string, password string) (*models.UserResponse, error) {
+
+	var exists bool
+
+	err := s.db.QueryRowContext(ctx, "SELECT EXISTS(SELECT 1 FROM Users WHERE email = $1)", email).Scan(&exists)
+	if err != nil {
+		return nil, err
+	}
+	if exists {
+		return nil, ErrEmailExists //fmt.Errorf("email already exists")
+	}
+	//create user
+	user, err := models.NewUser(id, firstname, lastname, email, password)
+	if err != nil {
+		return nil, err
+	}
+
+	//insert into db
+	query := "INSERT INTO Users(userid,firstname,lastname,email,password,createdat,updatedat) VALUES($1,$2,$3,$4,$5,$6,$7)"
+
+	_, err = s.db.ExecContext(ctx, query, user.ID, user.FirstName, user.LastName, user.Email, user.Password, user.CreatedAT, user.UpdatedAT)
+	if err != nil {
+		return nil, err
+	}
+	return &models.UserResponse{
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+		Email:     user.Email,
+		CreatedAt: user.CreatedAT,
+	}, nil
 }
