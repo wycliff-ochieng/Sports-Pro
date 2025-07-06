be7067d80e7d9c0595526545a81bd5aa40dd203e
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-07-06 16:10:20 +0300

    server entrypoint

--- auth-service/cmd/main.go
+++ auth-service/cmd/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	cmd "sports/authservice/cmd/api"
+	"sports/authservice/internal/config"
+)
 
 func main() {
 	fmt.Println("<<<<working on user service")
+
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("failed to load:%v", err)
+	}
+	server := cmd.NewAPIServer(":8000", cfg)
+	server.Run()
 }
