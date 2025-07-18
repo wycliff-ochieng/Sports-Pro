f2e626e4393a746f01c9413360ae7bc97455b930
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-07-18 19:53:35 +0300

    create user profile from consumed events

--- user-service/internal/service/user.go
+++ user-service/internal/service/user.go
@@ -1,16 +1,35 @@
 package service
 
 import (
-	"github.com/wycliff-ochieng/internal/models"
+	"context"
+	"fmt"
+
+	"github.com/wycliff-ochieng/internal/database"
 )
 
-type UserService struct{}
-
-func NewUser() *UserService {
-	return &UserService{}
+type UserService struct {
+	db database.DBInterface
 }
 
-func (u *UserService) CreateUserProfile(userID int, email string) (*models.Profile, error) {
+type EventsData struct {
+	UserID int    `json:"userid"`
+	Email  string `json:"email"`
+}
 
-	return &models.Profile{}, nil
+func NewUser(db database.DBInterface) *UserService {
+	return &UserService{db}
+}
+
+func (u *UserService) CreateUserProfile(ctx context.Context, userID int, email string) error {
+
+	var event EventsData
+
+	query := `INSERT INTO Users(userid,email) VALUES($1,$2)`
+
+	_, err := u.db.ExecContext(ctx, query, event.UserID, event.Email)
+	if err != nil {
+		return fmt.Errorf("something happened: %v", err)
+	}
+
+	return nil
 }
