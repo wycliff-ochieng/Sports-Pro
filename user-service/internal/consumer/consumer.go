e936199ba8aa8bfdeeccb2116b02ccad123d761f
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-07-19 11:07:01 +0300

    start consumer method

--- user-service/internal/consumer/consumer.go
+++ user-service/internal/consumer/consumer.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/wycliff-ochieng/internal/service"
 )
 
 type UserEventCreated struct {
@@ -15,13 +16,99 @@ type UserEventCreated struct {
 }
 
 type UserEventConsumer struct {
-	l *log.Logger
+	l        *log.Logger
+	u        *service.UserService
+	consumer *kafka.Consumer
 }
 
-func NewUserEventConsumer(l *log.Logger) *UserEventConsumer {
-	return &UserEventConsumer{l}
+func NewUserEventConsumer(l *log.Logger, u *service.UserService, bootstrapServers string, groupID string) (*UserEventConsumer, error) {
+
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrapservers":  bootstrapServers,
+		"group.id":          groupID,
+		"auto.offset.reset": "smallest",
+	})
+	if err != nil {
+		log.Fatalf("setting up consumer hitch:%v", err)
+	}
+
+	return &UserEventConsumer{
+		l:        l,
+		u:        u,
+		consumer: consumer,
+	}, nil
 }
 
+func (c *UserEventConsumer) StartConsumer(ctx context.Context, topic string) {
+
+	err := c.consumer.Subscribe(topic, nil)
+	if err != nil {
+		log.Fatalf("error subscribing to topic: %v", err)
+	}
+
+	for {
+		select {
+		//check if context has been cancelled, to enable shutting down gracefull
+		case <-ctx.Done():
+			c.l.Println("cancelled, shutting down")
+			return
+
+		//if context is not cancelled ,continue with polling
+		default:
+			ev := c.consumer.Poll(100)
+			if ev == nil {
+				continue //no event poll again
+			}
+			switch e := ev.(type) {
+			case *kafka.Message:
+				c.l.Printf("consumed messge from topic %s [%d] at offset %v", *e.TopicPartition.Topic, e.TopicPartition.Partition, e.TopicPartition.Offset)
+
+				var event UserEventCreated
+
+				if err := json.Unmarshal(e.Value, &event); err != nil {
+					c.l.Printf("error decoding event data:%v", err)
+
+					c.consumer.CommitMessage(e)
+
+					continue
+				}
+				//call database /create profile service
+				//database operation context
+				opCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
+				err := c.u.CreateUserProfile(opCtx, event.UserID, event.Email)
+				if err != nil {
+					log.Fatalf("")
+				}
+				cancel()
+
+				if err != nil {
+					c.l.Printf("some error when creating user profile %d:%v", event.UserID, err)
+				} else {
+					c.l.Printf("successfully created user:%d", event.UserID)
+
+					_, err := c.consumer.CommitMessage(e)
+					if err != nil {
+						c.l.Printf("error commit event message:%v", err)
+					}
+				}
+
+			case *kafka.Error:
+				//handling errors from the kafka brokers
+				c.l.Printf("Kafka Error: %v(code:%d)", e, e.Code())
+				if e.IsFatal() {
+					c.l.Println("something Fatal")
+					return
+				}
+			default:
+				c.l.Println("event ignored")
+			}
+
+		}
+
+	}
+}
+
+/*
 func (c *UserEventConsumer) StartMessageConsume(ctx context.Context, timeout time.Duration, consumer *kafka.Consumer) {
 	c.l.Println(">>> consuming topic messages started successsfully")
 
@@ -38,15 +125,17 @@ func (c *UserEventConsumer) StartMessageConsume(ctx context.Context, timeout tim
 		}
 		c.l.Printf("message has been received successfully from: %v", message.TopicPartition.Offset)
 
+		//handler
+
 		var event *UserEventCreated
 
 		if err := json.Unmarshal(message.Value, &event); err != nil {
-			c.l.Printf("failed to unmarshal the event message: %v",err)
+			c.l.Printf("failed to unmarshal the event message: %v", err)
 
 			consumer.CommitMessage(message)
 		}
 
-		//call user service to create profile >> TODO tomorrow connect to database 
+		//call user service to create profile >> TODO tomorrow connect to database
 		//profile,err := c.up.CreateUserProfile(ctx, event.userID,event.Email)
 
 	}
@@ -56,8 +145,9 @@ func (c *UserEventConsumer) StartMessageConsume(ctx context.Context, timeout tim
 //kafka reader
 
 //func (c *UserEventConsumer) StartReader(ctx context.Context, reader *kafka.Reader)
-
-func InitKafkaConsumer() (*kafka.Consumer, error) {
+/*
+func InitKafkaConsumer(topic string)error {
+	//topic := "profiles"
 	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers": "localhost:9092",
 		"group.id":          "foo",
@@ -66,5 +156,20 @@ func InitKafkaConsumer() (*kafka.Consumer, error) {
 	if err != nil {
 		log.Fatalf("something unexpected: %s", err)
 	}
-	return consumer, nil
+
+	err = consumer.Subscribe(topic, nil)
+	if err != nil {
+		return err
+	}
+
+	for {
+		ev := consumer.Poll(100)
+		switch e := ev.(type) {
+		case *kafka.Message:
+			fmt.Printf("consumed user created message from%v:",e.Value)
+		case *kafka.Error:
+			fmt.Printf("something happenned while consuming data:%v",e)
+		}
+	}
 }
+*/
