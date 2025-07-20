cf941c9d830012088964cf24d9aa71f884e1d569
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-07-20 20:13:49 +0300

    adding user db connection and configuration

--- user-service/internal/database/database.go
+++ user-service/internal/database/database.go
@@ -3,17 +3,57 @@ package database
 import (
 	"context"
 	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/pressly/goose/v3"
+	"github.com/wycliff-ochieng/internal/config"
 )
 
 type DBInterface interface {
-	QueryRowContext(ctx context.Context, query string, args ...interface{})
+	//QueryRowContext(ctx context.Context, query string, args ...interface{})
 	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
 }
 
 type Postgres struct {
-	db *sql.DB
+	db  *sql.DB
+	cfg *config.Config
 }
 
-func Newpostgres(db *sql.DB) *Postgres {
-	return &Postgres{db}
+func Newpostgres(cfg *config.Config) (*Postgres, error) {
+
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%d/%s?sslmode=%s",
+		cfg.DBUser,
+		cfg.DBPassword,
+		cfg.DBHost,
+		cfg.DBPort,
+		cfg.DBName,
+		cfg.DBsslmode,
+	)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+	//defer db.Close()
+	//set goose dialect
+	if err := goose.SetDialect("postgres"); err != nil {
+		log.Fatalf("error setting dialect:%v", err)
+	}
+
+	if err := goose.Up(db, "internal/database/migrations"); err != nil {
+		log.Fatalf("error spinning up goose:%v", err)
+	}
+
+	log.Println(">>>>migrations run successfully...")
+	return &Postgres{db: db}, nil
+}
+
+//func (p *Postgres) QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row {
+//	return p.db.QueryRowContext(ctx, query, args...)
+//}
+
+func (p *Postgres) ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error) {
+	return p.db.ExecContext(ctx, query, args...)
 }
