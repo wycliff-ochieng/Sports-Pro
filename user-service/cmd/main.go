d4d741da67cae257e3dbd510b8429804eaecd502
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-07-22 12:42:30 +0300

    server entrypoint

---
+++ user-service/cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/wycliff-ochieng/cmd/api"
+	"github.com/wycliff-ochieng/internal/config"
+)
+
+func main() {
+	fmt.Println("<<<<working on authentication service")
+
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("failed to load:%v", err)
+	}
+	server := api.NewAPIServer(":8000", cfg)
+	server.Run()
+}
