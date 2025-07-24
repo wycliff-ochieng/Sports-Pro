97a58ffd35b73e8e0335fd006147db7668005fdc
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-07-24 18:07:02 +0300

    getbyid,updateprofile handlers

--- user-service/internal/handlers/handlers.go
+++ user-service/internal/handlers/handlers.go
@@ -1,18 +1,57 @@
 package handlers
 
 import (
+	"context"
+	"encoding/json"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/wycliff-ochieng/internal/service"
 )
 
 type UserHandler struct {
 	l *log.Logger
+	p service.Profile
 }
 
-func NewUserHandler(l *log.Logger) *UserHandler {
-	return &UserHandler{l}
+type UpdatePofileReq struct {
+	UserID    int
+	Firstname string
+	Lastname  string
+	avatar    string
+	Email     string
+	//Createdat string
+	Updatedat time.Time
 }
 
-func (u *UserHandler) UpdateUserProfile(w http.ResponseWriter, r *http.Request) {}
+func NewUserHandler(l *log.Logger, p service.Profile) *UserHandler {
+	return &UserHandler{
+		l: l,
+		p: p,
+	}
+}
 
-func (u *UserHandler) GetProfile(w http.ResponseWriter, r *http.Request) {}
+func (u *UserHandler) GetUserProfile(w http.ResponseWriter, r *http.Request) {}
+
+func (u *UserHandler) UpdateUserProfile(w http.ResponseWriter, r *http.Request) {
+	u.l.Println(">>>updating user profile")
+
+	ctx := context.WithCancel(r.context(),)
+
+	var user *UpdatePofileReq
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "ERROR:unmarshaling data", http.StatusInternalServerError)
+	}
+
+	if user.Firstname == "" || user.Lastname == "" {
+		http.Error(w, "please enter a valid name", http.StatusBadRequest)
+	}
+
+	profile, err := u.p.UpdateUserProfile(ctx, user.UserID, user.Firstname, user.Lastname,user.avatar,user.Email, user.Updatedat)
+
+	w.Header().Add("Content-Type", "Application/json")
+	json.NewEncoder(w).Encode(&profile)
+}
