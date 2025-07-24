5c3ac35a7f90f87864b128a42c96c708ba2baf46
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-07-24 18:04:37 +0300

    updating user by id and getting user

--- user-service/internal/service/user.go
+++ user-service/internal/service/user.go
@@ -2,15 +2,25 @@ package service
 
 import (
 	"context"
+	//"database/sql"
+	"errors"
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/wycliff-ochieng/internal/database"
+	"github.com/wycliff-ochieng/internal/models"
 )
 
-type Profile interface{}
+type Profile interface {
+	GetProfileByID(int) (*models.Profile, error)
+	UpdateUserProfile(ctx context.Context, userID int, firstname, lastname, email, avatar string, updatedat time.Time) (*models.Profile, error)
+}
 
 type UserService struct {
+	l  *log.Logger
 	db database.DBInterface
+	//dbTx *sql.DB
 }
 
 type EventsData struct {
@@ -18,9 +28,18 @@ type EventsData struct {
 	Email  string `json:"email"`
 }
 
-func NewUserService(db database.DBInterface) *UserService {
-	return &UserService{db}
+func NewUserService(l *log.Logger, db database.DBInterface) *UserService {
+	return &UserService{
+		db: db,
+		l:  l,
+		//dbTx:dbTx,
+	}
 }
+
+var (
+	ErrNotFound     = errors.New("User Id not found")
+	ErrUpdateFailed = errors.New("Failed to update user profile")
+)
 
 func (u *UserService) CreateUserProfile(ctx context.Context, userID int, email string) error {
 
@@ -36,15 +55,41 @@ func (u *UserService) CreateUserProfile(ctx context.Context, userID int, email s
 	return nil
 }
 
-func (u *UserService) GetProfileByID(ctx context.Context,userID int) error{
+func (u *UserService) GetProfileByID(ctx context.Context, userID int) error {
 	var event EventsData
-	
+
 	query := "SELECT * from profiles WHERE id=$1"
 
-	_, err := u.db.ExecContext(ctx, query,event.UserID)
-	if err != nil{
+	_, err := u.db.ExecContext(ctx, query, event.UserID)
+	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func (u *UserService) UpdateUserProfile(ctx context.Context, userID int, firstname string, lastname string, email string, avatar string) (*models.Profile, error) {
+
+	u.l.Printf("UPDATING USER PROFILE UNDERWAY FOR:%v", userID)
+
+	//var exists bool
+
+	//err := u.db.QueryRowContext(ctx, "SELECT EXISTS(SELECT 1 FROM profiles WHERE EMAIL=$1)").Scan(&exists)
+	//if err != nil {
+	//	return nil, fmt.Errorf("errorr checking if email exists:%v", err)
+	//}
+
+	// Begin transaction
+	transaction, err := u.db.BeginTx(ctx, nil)
+	if err != nil {
+		u.l.Printf("Transaction undergone some errors:%v", err)
+	}
+
+	//defer rollback - if anything below panicks everything will be roollback
+	defer transaction.Rollback()
+
+	//check if user exists, call get user by ID
+
+	//request data ID
+	return nil, nil
 }
