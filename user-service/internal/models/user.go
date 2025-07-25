f85ee7b55a9801d78b9cc92a140f9b1d06a3f652
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-07-25 20:20:20 +0300

    profile struct cleanup

--- user-service/internal/models/user.go
+++ user-service/internal/models/user.go
@@ -1,13 +1,25 @@
 package models
 
+import "time"
+
 type Profile struct {
-	UserID int    `json:"userid"`
-	Email  string `json:"email"`
+	UserID    int    `json:"userid"`
+	Firstname string `json:"fullname"`
+	Lastname  string `json:"lastname"`
+	Email     string `json:"email"`
+	Avatar    string `json:"avatar"`
+	Createdat time.Time
+	Updatedat time.Time
 }
 
-func NewProfile(userid int, email string) *Profile {
+func NewProfile(userid int, firstname, lastname string, email string, avatar string, createdat time.Time, updatedat time.Time) *Profile {
 	return &Profile{
-		UserID: userid,
-		Email:  email,
+		UserID:    userid,
+		Firstname: firstname,
+		Lastname:  lastname,
+		Email:     email,
+		Avatar:    avatar,
+		Createdat: createdat,
+		Updatedat: updatedat,
 	}
 }
