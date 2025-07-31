9464da10c9ee84fb95b6a626109cba9909b3c125
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-07-31 20:05:45 +0300

    json validation

--- user-service/internal/models/user.go
+++ user-service/internal/models/user.go
@@ -7,18 +7,18 @@ type Profile struct {
 	Firstname string `json:"fullname"`
 	Lastname  string `json:"lastname"`
 	Email     string `json:"email"`
-	Avatar    string `json:"avatar"`
+	//Avatar    string `json:"avatar"`
 	Createdat time.Time
 	Updatedat time.Time
 }
 
-func NewProfile(userid int, firstname, lastname string, email string, avatar string, createdat time.Time, updatedat time.Time) *Profile {
+func NewProfile(userid int, firstname, lastname string, email string,createdat time.Time, updatedat time.Time) *Profile {
 	return &Profile{
 		UserID:    userid,
 		Firstname: firstname,
 		Lastname:  lastname,
 		Email:     email,
-		Avatar:    avatar,
+		//Avatar:    avatar,
 		Createdat: createdat,
 		Updatedat: updatedat,
 	}
