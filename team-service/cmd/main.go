2376f64c3a942c4d4360e6db2534d6251ebd669a
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-08-09 13:25:28 +0300

    server setup

--- team-service/cmd/main.go
+++ team-service/cmd/main.go
@@ -1,3 +1,21 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"github/wycliff-ochieng/cmd/api"
+	"github/wycliff-ochieng/internal/config"
+	"log"
+)
+
+func main() {
+	fmt.Println("Main function:::")
+
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("ERROR loading ENV variables:%v", err)
+	}
+
+	server := api.NewAPIServer(":3000", cfg)
+	server.Run()
+
+}
