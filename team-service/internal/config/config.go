0cc01e62bc6c164890875829aaffd3f75d57e984
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-08-09 10:42:14 +0300

    create team service configuration

---
+++ team-service/internal/config/config.go
@@ -0,0 +1,57 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBHost     string
+	DBPort     int
+	DBPassword string
+	DBName     string
+	DBUser     string
+	DBsslmode  string
+
+	JWTSecret     string
+	JWTExpiry     string
+	RefreshSecret string
+	RefreshExpiry string
+}
+
+func Load() (*Config, error) {
+	if err := godotenv.Load(); err != nil {
+		fmt.Printf("Warning, failed to load env varivales: %v", err)
+	}
+
+	config := &Config{}
+
+	config.DBHost = getEnv("DB_HOST", "localhost")
+	config.DBPort = getEnvAsInt("DB_PORT", 5433)
+	config.DBPassword = getEnv("DB_PASSWORD", "admin123")
+	config.DBName = getEnv("DB_NAME", "Teams")
+	config.DBUser = getEnv("DB_USER", "admin")
+	config.DBsslmode = getEnv("DB_SSLMODE", "disable")
+	//config.JWTSecret = getEnv("JWT_SECRET", "mydogiscalledrufus")
+	//config.RefreshSecret = getEnv("REFRESH_SECRET", "myotherdogiscalledseedolf")
+
+	return config, nil
+}
+
+func getEnv(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
+}
+
+func getEnvAsInt(key string, defaultValue int) int {
+	valueStr := getEnv(key, "")
+	if value, err := strconv.Atoi(valueStr); err == nil {
+		return value
+	}
+	return defaultValue
+}
