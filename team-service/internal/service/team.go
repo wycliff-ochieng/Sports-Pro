22e18f5a87b98074d2cbe26623d49f390a72f6f9
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-08-09 13:22:47 +0300

    create user service

--- team-service/internal/service/team.go
+++ team-service/internal/service/team.go
@@ -1,11 +1,10 @@
 package service
 
 import (
+	"context"
 	"github/wycliff-ochieng/internal/database"
 	"github/wycliff-ochieng/internal/models"
-	"time"
-
-	"github.com/google/uuid"
+	"log"
 )
 
 type TeamService struct {
@@ -16,6 +15,25 @@ func NewTeamService(db database.DBInterface) *TeamService {
 	return &TeamService{db}
 }
 
-func (ts *TeamService) CreateTeam(id uuid.UUID, name string, sport string, description string, createdat time.Time, updatedat time.Time) (*models.Team, error) {
-	return nil, nil
+func (ts *TeamService) CreateTeam(ctx context.Context, name string, sport string, description string) (*models.Team, error) {
+
+	var team *models.Team
+
+	team, err := models.NewTeam(name, sport, description)
+	if err != nil {
+		log.Fatalf("error creating new team")
+	}
+
+	query := `INSERT INTO teams(id,name,sports,description,createdat,updatedat) VALUES($1,$2,$3,$4,$5,$6)`
+
+	_, err = ts.db.ExecContext(ctx, query, team.TeamID, team.Name, team.Sport, team.Description, team.Createdat, team.Updatedat)
+	if err != nil {
+		log.Printf("ERROR creating team due to: %v", err)
+	}
+
+	return &models.Team{
+		Name:        name,
+		Sport:       sport,
+		Description: description,
+	}, nil
 }
