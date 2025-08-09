4d452e17f9f3d13e8e546c2c0ff504f976f9af86
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-08-09 13:23:14 +0300

    create team handler func

--- team-service/internal/handlers/handlers.go
+++ team-service/internal/handlers/handlers.go
@@ -1,14 +1,23 @@
 package handlers
 
 import (
+	"context"
+	"encoding/json"
 	"github/wycliff-ochieng/internal/service"
 	"log"
 	"net/http"
+	"time"
 )
 
 type TeamHandler struct {
 	l *log.Logger
 	t *service.TeamService
+}
+
+type createTeamReq struct {
+	Name        string
+	Sport       string
+	Description string
 }
 
 func NewTeamHandler(l *log.Logger, t *service.TeamService) *TeamHandler {
@@ -19,5 +28,29 @@ func NewTeamHandler(l *log.Logger, t *service.TeamService) *TeamHandler {
 }
 
 func (h *TeamHandler) CreateTeam(w http.ResponseWriter, r *http.Request) {
-	return
+
+	h.l.Println("CREATE TEAM HANDLER NOW TOUCHED")
+
+	ctx, cancel := context.WithTimeout(r.Context(), 5*time.Second)
+	defer cancel()
+
+	var create *createTeamReq
+
+	err := json.NewDecoder(r.Body).Decode(&create)
+	if err != nil {
+		h.l.Println("ERROR while marshaling the data")
+		http.Error(w, "Error Decoding request input", http.StatusInternalServerError)
+		return
+	}
+
+	team, err := h.t.CreateTeam(ctx, create.Name, create.Sport, create.Description)
+	if err != nil {
+		h.l.Println("somethig is wrong in the service transaction")
+		http.Error(w, "FAILED:Error creating team service", http.StatusFailedDependency)
+		return
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(&team)
 }
