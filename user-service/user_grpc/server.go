387b322abbb304cec3fbf6b33a2f3865317c528a
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-08-25 08:38:44 +0300

    calling user service to fetch profile list in bulk

--- user-service/user_grpc/server.go
+++ user-service/user_grpc/server.go
@@ -16,6 +16,19 @@ type Server struct {
 
 func (s *Server) GetUserProfiles(ctx context.Context, req *grpc.GetUserRequest) (*grpc.GetUserProfileResponse, error) {
 	s.Logger.Info("get profile for a user")
+
 	//call user service for profile list
+	profiles, err := s.service.GetUserProfilesByUUIDs(ctx, req.Userid)
+	if err != nil {
+		s.Logger.Error("Issue getting user profiles from user-service")
+		return nil, err
+	}
+
+	//convert Profiles struct to gRPC userProfile struct
+	grpcProfile := make(map[string]*grpc.UserProfile)
+	for _, p := range profiles {
+		grpcProfile[p.UserID.String()] = &grpc.UserProfile{}
+	}
+
 	return nil, nil
 }
