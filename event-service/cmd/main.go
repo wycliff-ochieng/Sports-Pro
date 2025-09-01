7debe89d7f650c6bc26038be41c08761115ea14a
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-09-01 15:44:44 +0300

    event service server entrypoint

---
+++ event-service/cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/wycliff-ochieng/cmd/api"
+	"github.com/wycliff-ochieng/internal/config"
+)
+
+func main() {
+	fmt.Println("Main function:::")
+
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("ERROR loading ENV variables:%v", err)
+	}
+
+	server := api.NewAPIServer(":3000", cfg)
+	server.Run()
+
+}
