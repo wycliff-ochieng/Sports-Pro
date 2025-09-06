0594c1e8af3abb39cbe5ccec6e2e36a5c535d02d
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-09-06 12:19:49 +0300

    create event ,prepoluating attendance list, setting stattus to pending

--- event-service/internal/service/events.go
+++ event-service/internal/service/events.go
@@ -2,7 +2,10 @@ package service
 
 import (
 	"context"
+	"database/sql"
 	"errors"
+	"fmt"
+	"log"
 	"log/slog"
 	"time"
 
@@ -57,9 +60,99 @@ func (es *EventService) CreateTeamEvent(ctx context.Context, reqUserID uuid.UUID
 	//check response/error from grRPC call- > if user is a member of that team or not and if member is coach/manager
 	membersInfo, found := members.Members[reqUserID.String()]
 	if !found {
-		es.l.Error("user does not exist in the team service")
+		es.l.Error("user does not exist in the team therefore is not a member")
 		return nil, ErrForbidden
 	}
 
+	isAuthorized := membersInfo.Role == "coach" || membersInfo.Role == "manager"
+	if !isAuthorized {
+		es.l.Warn("user is not a coach/manager therefore cant create events")
+	}
+	es.l.Info("Authorization is successfull")
+
+	//get team data for attendance table insert(business logic)
+	teamMembersReq := team_proto.GetTeamSummaryRequest{TeamId: CreateEventReq.TeamID.String()}
+	teamMembersRes, err := es.teamClient.GetTeamSummary(ctx, &teamMembersReq)
+	if err != nil {
+		es.l.Error("gRPC call to team service failed")
+		return nil, fmt.Errorf("cause of failure: %v", err)
+	}
+
+	teamMembers := teamMembersRes.Members
+
+	//database atomic transaction ->
+
+	//start transactions
+	txs, err := es.db.BeginTx(ctx, nil)
+	if err != nil {
+		es.l.Error("Issue with transaction start up")
+		return nil, err
+	}
+
+	defer txs.Rollback()
+
+	newEvent := &models.Event{
+		TeamID:    CreateEventReq.TeamID,
+		Title:     CreateEventReq.Name,
+		EventType: CreateEventReq.EventType,
+		Location:  CreateEventReq.Location,
+		StartTime: CreateEventReq.StartTime,
+		EndTime:   CreateEventReq.EndTime,
+	}
+
+	createdEvent, err := es.CreateEvent(ctx, txs, newEvent.TeamID, newEvent.Title, newEvent.EventType, newEvent.Location, newEvent.StartTime, newEvent.EndTime)
+	if err != nil {
+		es.l.Error("error creating event due to ", err)
+	}
+	es.l.Info("event created successfully for team", createdEvent.TeamID)
+
+	//write to database the attendace list
+	//prepopulate the initial list
+	if len(teamMembers) > 0 {
+		var attendanceRecords []models.Attendance
+		for _, member := range teamMembers {
+			memberID, err := uuid.Parse(member.UserId)
+			if err != nil {
+				return nil, err
+			}
+
+			attendanceRecords = append(attendanceRecords, models.Attendance{
+				EventID:    createdEvent.ID,
+				UserID:     memberID,
+				Status:     "PENDING",
+				UpdateteAt: time.Now(),
+			})
+		}
+	}
+
 	return nil, nil
+}
+
+func (es *EventService) CreateEvent(ctx context.Context, tx *sql.Tx, teamID uuid.UUID, name string, eventtype string, location string, starttime, endtime time.Time) (*models.Event, error) {
+	es.l.Info("Create event database execution")
+
+	//var event *models.Event
+
+	newEvent, err := models.NewEvent(teamID, name, eventtype, location, starttime, endtime)
+	if err != nil {
+		es.l.Error("error creating new team event")
+		return nil, fmt.Errorf("due to :%v", err)
+	}
+
+	query := `INSERT INTO event(event_id,team_id,title,event_type,start_time,end_time) VALUES($1,$2,$3,$4,$5,$6)`
+
+	_, err = es.db.ExecContext(ctx, query, newEvent.TeamID, newEvent.Title, newEvent.EventType, newEvent.Location, newEvent.StartTime, newEvent.EventType)
+	if err != nil {
+		log.Fatalf("Error executing query:%v", err)
+		return nil, err
+	}
+	return &models.Event{
+		ID:        uuid.New(),
+		TeamID:    teamID,
+		Title:     name,
+		EventType: eventtype,
+		Location:  location,
+		StartTime: starttime,
+		EndTime:   endtime,
+	}, nil
 }
