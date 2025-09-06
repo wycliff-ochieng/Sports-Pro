7f089466b25e4b0db97c79dbcced86d0b06cf930
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-09-06 11:21:25 +0300

    add : field event type

--- event-service/internal/models/models.go
+++ event-service/internal/models/models.go
@@ -29,7 +29,19 @@ type Attendance struct {
 type CreateEventReq struct {
 	TeamID    uuid.UUID
 	Name      string
+	EventType string
 	Location  string
 	StartTime time.Time
 	EndTime   time.Time
+}
+
+func NewEvent(teamID uuid.UUID, name string, eventype string, Location string, start, end time.Time) (*Event, error) {
+	return &Event{
+		TeamID:    teamID,
+		Title:     name,
+		EventType: eventype,
+		Location:  Location,
+		StartTime: start,
+		EndTime:   end,
+	}, nil
 }
