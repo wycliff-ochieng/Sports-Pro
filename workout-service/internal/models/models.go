cf8ff95f6ba215167b58830046e90ec029a3156a
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-10-19 14:37:24 +0300

    add : models for the workout service

---
+++ workout-service/internal/models/models.go
@@ -0,0 +1,39 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Workout struct {
+	ID          uuid.UUID
+	Name        string
+	Description string
+	Category    string
+	CreatedBy   uuid.UUID //user_id of the coach / admin
+	CreatedOn   time.Time
+	UpdatedON   time.Time
+}
+
+type Exercise struct {
+	ID          uuid.UUID
+	Name        string
+	Description string
+	Instruction string
+	CreatedBy   uuid.UUID
+	CreatedOn   time.Time
+	UpdatedOn   time.Time
+}
+
+type Media struct {
+	ID             uuid.UUID
+	ParentID       uuid.UUID
+	PArentType     string
+	StorageProvide string
+	BucketName     string
+	ObjectKey      string
+	Filename       string
+	MimeType       string
+	UploadedAT     time.Time
+}
