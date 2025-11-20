12f78fb60cbcecc42385fb8977ffc022d40da144
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-11-20 23:25:39 +0300

    feat: grpc address/network connection implementation

--- team-service/cmd/api/api.go
+++ team-service/cmd/api/api.go
@@ -6,14 +6,17 @@ import (
 	"github/wycliff-ochieng/internal/handlers"
 	internal "github/wycliff-ochieng/internal/producer"
 	"github/wycliff-ochieng/internal/service"
+	"net"
 
-	//"github/wycliff-ochieng/middleware"
+	rpc "github/wycliff-ochieng/grpc"
+
 	"log"
 	"log/slog"
 	"net/http"
 	"os"
 
 	auth "github.com/wycliff-ochieng/sports-common-package/middleware"
+	"github.com/wycliff-ochieng/sports-common-package/team_grpc/team_proto"
 	"github.com/wycliff-ochieng/sports-common-package/user_grpc/user_proto"
 
 	"github.com/gorilla/mux"
@@ -57,7 +60,7 @@ func (s *APIServer) Run() {
 
 	ep := internal.NewUpdateTeam(p, "team_events")
 
-	userServiceAddress := "50051" // "user-service-svc:50051"  -> K8s name and grpc port
+	userServiceAddress := "localhost:50051" // "user-service-svc:50051"  -> K8s name and grpc port
 
 	conn, err := grpc.NewClient(userServiceAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
@@ -78,6 +81,40 @@ func (s *APIServer) Run() {
 	//set up router
 	router := mux.NewRouter()
 
+	//set up grpc server implementation
+	gRPCAddress := "50052"
+	lis, err := net.Listen("tcp", ":"+gRPCAddress)
+	if err != nil {
+		//handle this error
+		log.Fatalf("ERROR spinning up network listener due to: %v", err)
+	}
+
+	grpcServ := grpc.NewServer()
+
+	grpcServer := &rpc.Server{
+		Service: ts,
+		Logger:  l,
+	}
+
+	//user_proto.RegisterUserServiceRPCServer(grpcServ, grpcServer)
+	team_proto.RegisterTeamRPCServer(grpcServ, grpcServer)
+
+	//getUser := router.Methods("GET").Subrouter()
+	//getUser.HandleFunc("/grab", uh.GetUserProfile)
+
+	/*l.Printf("gRPC server listening o port: %v", gRPCAddress)
+	if err := grpcServ.Serve(lis); err != nil {
+		log.Fatalf("Some error spinning up RPC server: %v", err)
+		os.Exit(1)
+	}*/
+
+	go func() {
+		l.Printf("gRPC server starting on port: %v", gRPCAddress)
+		if err := grpcServ.Serve(lis); err != nil {
+			log.Fatalf("Fatal error: gRPC server failed to serve: %v", err)
+		}
+	}()
+
 	//routes
 	createTeam := router.Methods("POST").Subrouter()
 	createTeam.HandleFunc("/api/teams", th.CreateTeam)
@@ -93,23 +130,25 @@ func (s *APIServer) Run() {
 	getTeamsByID.HandleFunc("/api/team/{team_id}", th.GetTeamsByID)
 
 	updateTeam := router.Methods("PUT").Subrouter()
-	updateTeam.HandleFunc("/api/team/{teamid}/update", th.UpdateTeam)
-	updateTeam.Use(auth.RequireRole("COACH", "MANAGER"))
+	updateTeam.HandleFunc("/api/team/{team_id}/update", th.UpdateTeam)
+	updateTeam.Use(authMiddleware)
+	updateTeam.Use(auth.RequireRole("coach", "manager", "player"))
 	//updateTeam.Use(middleware.UserMiddlware(s.cfg.JWTSecret))
 
 	addMember := router.Methods("POST").Subrouter()
-	addMember.HandleFunc("/api/team/{teamid}/add", th.AddTeamMember)
-	addMember.Use(auth.RequireRole("coach", "manager"))
+	addMember.HandleFunc("/api/team/{team_idid}/add", th.AddTeamMember)
+	addMember.Use(authMiddleware)
+	addMember.Use(auth.RequireRole("coach", "manager", "player"))
 
 	getTeamList := router.Methods("GET").Subrouter()
-	getTeamList.HandleFunc("/api/team/{teamid}/members", th.GetTeamRoster)
+	getTeamList.HandleFunc("/api/team/{team_id}/members", th.GetTeamRoster)
 
 	updateTeamMember := router.Methods("PUT").Subrouter()
-	updateTeamMember.HandleFunc("/api/team/{teamid}/members/{userid}/update", th.UpdateTeamMember)
+	updateTeamMember.HandleFunc("/api/team/{teamid}/members/{user_id}/update", th.UpdateTeamMember)
 	updateTeamMember.Use(auth.RequireRole("coach", "manager"))
 
 	deleteTeamMember := router.Methods("DELETE").Subrouter()
-	deleteTeamMember.HandleFunc("/api/team/{teamid}/member/{userid}/delete", th.RemoveTeamMember)
+	deleteTeamMember.HandleFunc("/api/team/{teamid}/member/{user_id}/delete", th.RemoveTeamMember)
 	deleteTeamMember.Use(auth.RequireRole("coach", "manager"))
 
 	if err := http.ListenAndServe(s.addrr, router); err != nil {
