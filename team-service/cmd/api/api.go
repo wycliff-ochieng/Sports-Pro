7440171b0514d8ede1d3ff3862e7a7f3c036fd99
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-11-22 19:04:39 +0300

    feat: grpc addreess as in .env and fallback

--- team-service/cmd/api/api.go
+++ team-service/cmd/api/api.go
@@ -62,7 +62,12 @@ func (s *APIServer) Run() {
 
 	ep := internal.NewUpdateTeam(p, "team_events")
 
-	userServiceAddress := "localhost:50051" // "user-service-svc:50051"  -> K8s name and grpc port
+	userServiceAddress := os.Getenv("USER_SERVICE_GRPC_ADDR")
+	if userServiceAddress == "" {
+		userServiceAddress = "localhost:50051"
+	}
+
+	//userServiceAddress := "localhost:50051" // "user-service-svc:50051"  -> K8s name and grpc port
 
 	conn, err := grpc.NewClient(userServiceAddress, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
